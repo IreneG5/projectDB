ae64c9a8e33d07dd6f906f9025e62257e06bb596
Irene <irene.g5555@gmail.com>
2016-11-12 21:40:09 +0000

    Add code to use the driver class to perform all major database operations. Selecting rows, columns, concat & sum, and inserting, updating and deleting information.

--- main.py
+++ main.py
@@ -7,10 +7,49 @@ Retrieve the settings from the
 our database so we can instantiate our
 MySQLDatabase object
 """
-db = MySQLDatabase(db_config.get('db_name'),
-                   db_config.get('user'),
-                   db_config.get('pass'),
-                   db_config.get('host'))
+if __name__ == "__main__":
+    db = MySQLDatabase(db_config.get('db_name'),
+                       db_config.get('user'),
+                       db_config.get('pass'),
+                       db_config.get('host'))
+
+    # Get all the records from
+    # the people table
+    results = db.select('people')
+    print "Selecting Rows"
+    for row in results:
+        print row
+
+    # Selecting columns with named tuples
+    results = db.select('people',
+                        columns=['id', 'first_name'], named_tuples=True)
+    print "Selecting Columns"
+    for row in results:
+        print row.id, row.first_name
+
+        # We can also do more complex queries using `CONCAT`
+        # and `SUM`
+    people = db.select('people', columns=["CONCAT(first_name, ' ', second_name) "
+                        " AS full_name", "SUM(amount)AS total_spend"],
+                        named_tuples=True, where="people.id=1",
+                        join="orders ON people.id=orders.person_id")
+    print ("CONCAT & SUM")
+    for person in people:
+        print person.full_name, "spent ", person.total_spend
+
+    # Inserting an order
+    db.insert('orders', person_id="2", amount="120.00")
+
+    # Updating a person
+    person = db.select('people', named_tuples=True)[0]
+
+    db.update('profiles', where="person_id=%s" % person.id,
+          address="1a another street")
+
+    # Deleting a record
+    person = db.select('people', named_tuples=True)[0]
+    db.delete('orders', person_id="=%s" % person.id, id="=1")
+
 
 # Get all the available tables for
 # our database and print them out.
